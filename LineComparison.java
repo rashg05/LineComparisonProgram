693c29abf419a057ee7fc5921426caabdf73440f
rashg05 <gajra.rashmi0504@gmail.com>
2022-01-20 11:57:23 +0530

    Added: Welcome Message for Line Comparison

---
+++ LineComparison.java
@@ -0,0 +1,12 @@
+package Assignment;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Line\r\n"
+				+ "Comparison Computation\r\n"
+				+ "Program on Master Branch");
+	}
+
+}
